fe5685333cf3a8ce2cbeba19eec5a62306800db1
tyflow <jshi25@naver.com>
2023-03-31 15:36:10 +0900

    추가 커밋

---
+++ src/lib/observer.ts
@@ -0,0 +1,33 @@
+import {Dispatch, SetStateAction} from 'react';
+
+const observerOption = {
+    threshold: 0.4,
+    rootMargin: '-60px 0px 0px 0px',
+};
+
+export const getIntersectionObserver = (setState: Dispatch<SetStateAction<string>>) => {
+    let direction = '';
+    let prevYposition = 0;
+
+    // scroll 방향 check function
+    const checkScrollDirection = (prevY: number) => {
+        if (window.scrollY === 0 && prevY === 0) return;
+        else if (window.scrollY > prevY) direction = 'down';
+        else direction = 'up';
+
+        prevYposition = window.scrollY;
+    };
+
+    // observer
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            checkScrollDirection(prevYposition);
+            if ((direction === 'down' && !entry.isIntersecting) ||
+                (direction === 'up' && entry.isIntersecting)) {
+                setState(entry.target.innerHTML);
+            }
+        });
+    }, observerOption);
+
+    return observer;
+}
